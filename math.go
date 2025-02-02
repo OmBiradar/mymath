7b80f03dae514bc393335fb7f995e7645c04aa7c
OmBiradar <ombiradar04@gmail.com>
2025-02-02 22:49:39 +0530

    Added a func to Substract 2 numbers

--- math.go
+++ math.go
@@ -1,5 +1,11 @@
 package mymath
 
+// Add returns the sum of a and b.
 func Add(a, b int) int {
 	return a + b
+}
+
+// Subtract returns the difference between a and b.
+func Subtract(a, b int) int {
+	return a - b
 }
--- math_test.go
+++ math_test.go
@@ -8,3 +8,10 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected 5, got %d", result)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	result := Subtract(3, 2)
+	if result != 1 {
+		t.Errorf("Expected 1, got %d", result)
+	}
+}
